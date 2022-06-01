a1fb5f61b7ef7ee4c3499fc4c8e4eca6103e41b9
kiri0314 <t.kirihara0314@gmail.com>
2022-06-01 23:51:27 +0900

    feat: t_game_data構造体を定義

--- include/cub3d.h
+++ include/cub3d.h
@@ -6,6 +6,14 @@
 # include "libft.h"
 # include "mlx.h"
 
+typedef struct s_game_data
+{
+	char	*no_path;
+	char	*so_path;
+	char	*we_path;
+	char	*ea_path;
+}	t_game_data;
+
 void	check_arg(int ac, char **av);
 void	finish_error(char *error_message);
 

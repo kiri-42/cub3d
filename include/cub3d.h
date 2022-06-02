ea4f7d477a5b7905a183348a349207ba6a8c0e4c
kiri0314 <t.kirihara0314@gmail.com>
2022-06-02 13:37:05 +0900

    feat: t_game_dataを定義

--- include/cub3d.h
+++ include/cub3d.h
@@ -5,6 +5,29 @@
 # include "libft.h"
 # include "mlx.h"
 
+typedef struct s_texture_path
+{
+	char	*no_path;
+	char	*so_path;
+	char	*we_path;
+	char	*ea_path;
+}	t_texture_path;
+
+typedef struct s_rgb
+{
+	int	red;
+	int	green;
+	int	blue;
+}	t_rgb;
+
+typedef struct s_game_data
+{
+	t_texture_path	tp;
+	t_rgb			floor_color;
+	t_rgb			ceiling_color;
+	char			**map;
+}	t_game_data;
+
 void	check_arg(int ac, char **av);
 
 

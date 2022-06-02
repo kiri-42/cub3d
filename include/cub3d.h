b1c37c48b03ba952abec96cb47e05d0c60d018fb
kiri0314 <t.kirihara0314@gmail.com>
2022-06-02 14:29:51 +0900

    fix: 構造体の一部を変更

--- include/cub3d.h
+++ include/cub3d.h
@@ -11,12 +11,27 @@
 
 # define TEXTURE_LINE 4
 
-typedef struct s_game_data
+typedef struct s_texture_path
 {
 	char	*no_path;
 	char	*so_path;
 	char	*we_path;
 	char	*ea_path;
+}	t_texture_path;
+
+typedef struct s_rgb
+{
+	int	red;
+	int	green;
+	int	blue;
+}	t_rgb;
+
+typedef struct s_game_data
+{
+	t_texture_path	tp;
+	t_rgb			floor_color;
+	t_rgb			ceiling_color;
+	char			**map;
 }	t_game_data;
 
 void	check_arg(int ac, char **av);

c320155bef4b6996e1426bfb4adf493f5da8901c
kiri0314 <t.kirihara0314@gmail.com>
2022-06-06 02:46:56 +0900

    feat: free_cubfileを実装

--- src/main.c
+++ src/main.c
@@ -1,5 +1,18 @@
 #include "cub3d.h"
 
+void	free_cubfile(t_game_data *gd)
+{
+	size_t	i;
+
+	i = 0;
+	while (gd->cubfile[i] != NULL)
+	{
+		free(gd->cubfile[i]);
+		i++;
+	}
+	free(gd->cubfile);
+}
+
 int	main(int ac, char **av)
 {
 	t_game_data	gd;
@@ -7,6 +20,7 @@ int	main(int ac, char **av)
 	check_arg(ac, av);
 	read_cubfile(&gd, av[1]);
 	init_game_data(&gd);
+	free_cubfile(&gd);
 	// print_texture_path(&gd);
 	return (0);
 }

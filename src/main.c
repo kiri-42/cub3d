23e8c0a05979197ba6d54f43a2f6621c891ea141
kiri0314 <t.kirihara0314@gmail.com>
2022-06-06 19:00:01 +0900

    feat: separate_block関数を実装

--- src/main.c
+++ src/main.c
@@ -43,11 +43,84 @@ void	check_edge(t_game_data *gd)
 		}
 		i++;
 	}
+}
 
+void	check_space_block()
+{
+	printf("[space]");
+}
+
+void	check_zero_block()
+{
+	printf("[zero]");
+}
+
+void	check_one_block()
+{
+	printf("[one]");
+}
+
+void	check_north_block()
+{
+	printf("[north]");
+}
+
+void	check_south_block()
+{
+	printf("[south]");
+}
+
+void	check_west_block()
+{
+	printf("[west]");
+}
+
+void	check_east_block()
+{
+	printf("[east]");
+}
+
+void	separate_block(t_game_data *gd, size_t i, size_t j)
+{
+	if (gd->map[i][j] == ' ')
+		check_space_block();
+	else if (gd->map[i][j] == '0')
+		check_zero_block();
+	else if (gd->map[i][j] == '1')
+		check_one_block();
+	else if (gd->map[i][j] == 'N')
+		check_north_block();
+	else if (gd->map[i][j] == 'S')
+		check_south_block();
+	else if (gd->map[i][j] == 'W')
+		check_west_block();
+	else if (gd->map[i][j] == 'E')
+		check_east_block();
+	else
+		exit_error("Error");
+}
+
+void	check_block(t_game_data *gd)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	while (gd->map[i] != NULL)
+	{
+		j = 0;
+		while (gd->map[i][j] != '\0')
+		{
+			separate_block(gd, i, j);
+			j++;
+		}
+		i++;
+	}
 }
 
 void	check_map(t_game_data *gd)
 {
+	check_block(gd);
 	check_edge(gd);
 }
 

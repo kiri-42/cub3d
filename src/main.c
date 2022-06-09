5ef50cc246ae461a97581c2271b74939586caf75
kiri0314 <t.kirihara0314@gmail.com>
2022-06-10 02:16:32 +0900

    feat: dupmap関数を実装

--- src/main.c
+++ src/main.c
@@ -45,83 +45,143 @@ void	check_edge(t_game_data *gd)
 	}
 }
 
-void	check_space_block()
-{
-	printf("[space]");
-}
+// void	check_space_block(t_game_data *gd, size_t i, size_t j)
+// {
+// 	int	x;
+// 	int	y;
 
-void	check_zero_block()
-{
-	printf("[zero]");
-}
+// 	x = -1;
+// 	y = -1;
+// 	while (x <= 1)
+// 	{
+// 		if (i + x < 0)
+// 		{
+// 			x++;
+// 			continue;
+// 		}
+// 		while (y <= 1)
+// 		{
 
-void	check_one_block()
-{
-	printf("[one]");
-}
+// 			if (!(gd->map[i + x][j + y] == ' ' || gd->map[i + x][j + y] == '1'))
+// 				exit_error("Error")
+// 			y++;
+// 		}
+// 		x++;
+// 	}
 
-void	check_north_block()
-{
-	printf("[north]");
-}
+// 	printf("[space]");
+// }
 
-void	check_south_block()
-{
-	printf("[south]");
-}
+// void	check_zero_block(t_game_data *gd, size_t i, size_t j)
+// {
+// 	printf("[zero]");
+// }
 
-void	check_west_block()
-{
-	printf("[west]");
-}
+// void	check_one_block(t_game_data *gd, size_t i, size_t j)
+// {
+// 	printf("[one]");
+// }
 
-void	check_east_block()
-{
-	printf("[east]");
-}
+// void	check_north_block(t_game_data *gd, size_t i, size_t j)
+// {
+// 	printf("[north]");
+// }
 
-void	separate_block(t_game_data *gd, size_t i, size_t j)
-{
-	if (gd->map[i][j] == ' ')
-		check_space_block();
-	else if (gd->map[i][j] == '0')
-		check_zero_block();
-	else if (gd->map[i][j] == '1')
-		check_one_block();
-	else if (gd->map[i][j] == 'N')
-		check_north_block();
-	else if (gd->map[i][j] == 'S')
-		check_south_block();
-	else if (gd->map[i][j] == 'W')
-		check_west_block();
-	else if (gd->map[i][j] == 'E')
-		check_east_block();
-	else
-		exit_error("Error");
-}
+// void	check_south_block(t_game_data *gd, size_t i, size_t j)
+// {
+// 	printf("[south]");
+// }
 
-void	check_block(t_game_data *gd)
+// void	check_west_block(t_game_data *gd, size_t i, size_t j)
+// {
+// 	printf("[west]");
+// }
+
+// void	check_east_block(t_game_data *gd, size_t i, size_t j)
+// {
+// 	printf("[east]");
+// }
+
+// void	separate_block(t_game_data *gd, size_t i, size_t j)
+// {
+// 	if (gd->map[i][j] == ' ')
+// 		check_space_block(gd, i, j);
+// 	else if (gd->map[i][j] == '0')
+// 		check_zero_block();
+// 	else if (gd->map[i][j] == '1')
+// 		check_one_block();
+// 	else if (gd->map[i][j] == 'N')
+// 		check_north_block();
+// 	else if (gd->map[i][j] == 'S')
+// 		check_south_block();
+// 	else if (gd->map[i][j] == 'W')
+// 		check_west_block();
+// 	else if (gd->map[i][j] == 'E')
+// 		check_east_block();
+// 	else
+// 		exit_error("非対応の文字です");
+// }
+
+// void	check_block(t_game_data *gd)
+// {
+// 	size_t	i;
+// 	size_t	j;
+
+// 	i = 0;
+// 	while (gd->map[i] != NULL)
+// 	{
+// 		j = 0;
+// 		while (gd->map[i][j] != '\0')
+// 		{
+// 			separate_block(gd, i, j);
+// 			j++;
+// 		}
+// 		i++;
+// 	}
+// }
+
+size_t	get_line_size(char **map)
 {
 	size_t	i;
-	size_t	j;
 
 	i = 0;
-	while (gd->map[i] != NULL)
+	while (map[i] != NULL)
 	{
-		j = 0;
-		while (gd->map[i][j] != '\0')
-		{
-			separate_block(gd, i, j);
-			j++;
-		}
 		i++;
 	}
+	return (i);
+}
+
+char **dupmap(char **map)
+{
+	char	**dup;
+	size_t	line_size;
+	size_t	i;
+
+	line_size = get_line_size(map);
+	dup = (char **)x_malloc(sizeof(size_t) * (line_size + 1));
+	i = 0;
+	while (i < line_size)
+	{
+		dup[i] = ft_strdup(map[i]);
+		i++;
+	}
+	dup[i] = NULL;
+	return (dup);
+}
+
+void	check_wall(t_game_data *gd)
+{
+	char	**map;
+
+	map = dupmap(gd->map);
 }
 
 void	check_map(t_game_data *gd)
 {
-	check_block(gd);
+	// check_block(gd);
 	check_edge(gd);
+	check_wall(gd);
 }
 
 int	main(int ac, char **av)

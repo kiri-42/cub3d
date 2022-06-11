bcd5a3762b0eff25cb2a485c88c21c1c41584ffe
kiri0314 <t.kirihara0314@gmail.com>
2022-06-12 04:48:05 +0900

    feat: check_rgbを実装

--- src/is_initialized.c
+++ src/is_initialized.c
@@ -1,19 +1,19 @@
 #include "cub3d.h"
 
-void	is_initialized_rgb(t_rgb color)
-{
-	if (color.red == -1)
-		exit_error("red is uninitialized");
-	if (color.green == -1)
-		exit_error("green is uninitialized");
-	if (color.blue == -1)
-		exit_error("blue is uninitialized");
-}
+// void	is_initialized_rgb(t_rgb color)
+// {
+// 	if (color.red == -1)
+// 		exit_error("red is uninitialized");
+// 	if (color.green == -1)
+// 		exit_error("green is uninitialized");
+// 	if (color.blue == -1)
+// 		exit_error("blue is uninitialized");
+// }
 
 void	is_initialized(t_game_data *gd)
 {
-	is_initialized_rgb(gd->ceiling_color);
-	is_initialized_rgb(gd->floor_color);
+	// is_initialized_rgb(gd->ceiling_color);
+	// is_initialized_rgb(gd->floor_color);
 	if (gd->map == NULL)
 		exit_error("map is uninitialized");
 }

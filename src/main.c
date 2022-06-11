75590c5bcd755385f16d0ddcd6a2dd5e3768fe58
kiri0314 <t.kirihara0314@gmail.com>
2022-06-12 02:43:42 +0900

    feat: is_initialized_rgbを実装

--- src/main.c
+++ src/main.c
@@ -18,9 +18,22 @@ void	is_initialized_texture_path(t_texture_path *tp)
 		exit_error("ea_path is uninitialized");
 }
 
+void	is_initialized_rgb(t_rgb *color)
+{
+	printf("%d\n", color->red);
+	if (color->red == -1)
+		exit_error("red is uninitialized");
+	if (color->red == -1)
+		exit_error("green is uninitialized");
+	if (color->red == -1)
+		exit_error("blue is uninitialized");
+}
+
 void	is_initialized(t_game_data *gd)
 {
 	is_initialized_texture_path(&(gd->tp));
+	is_initialized_rgb(&(gd->ceiling_color));
+	is_initialized_rgb(&(gd->floor_color));
 }
 
 void	check_game_data(t_game_data *gd)

7dfac62921cb85db4cb9e7834d3817e2eab1f173
kiri0314 <t.kirihara0314@gmail.com>
2022-06-12 01:46:40 +0900

    feat: init_game_data_NULLを実装

--- src/main.c
+++ src/main.c
@@ -1,13 +1,44 @@
 #include "cub3d.h"
 
+// void	check_texture_path(t_game_data *gd)
+// {
+// 	(void)gd;
+// 	printf("texture_path\n");
+// }
+
+void	init_texture_path_NULL(t_texture_path *tp)
+{
+	tp->no_path = NULL;
+	tp->so_path = NULL;
+	tp->we_path = NULL;
+	tp->ea_path = NULL;
+}
+
+void	init_rgb(t_rgb *color)
+{
+	color->red = -1;
+	color->green = -1;
+	color->blue = -1;
+}
+
+void	init_game_data_NULL(t_game_data *gd)
+{
+	init_texture_path_NULL(&(gd->tp));
+	init_rgb(&(gd->ceiling_color));
+	init_rgb(&(gd->floor_color));
+	gd->map = NULL;
+}
+
 int	main(int ac, char **av)
 {
 	t_game_data	gd;
 
 	check_arg(ac, av);
+	init_game_data_NULL(&gd);
 	read_cubfile(&gd, av[1]);
 	init_game_data(&gd);
 	free_cubfile(&gd);
+	// check_texture_path(&gd);
 	check_map(&gd);
 	// print_texture_path(&gd);
 	return (0);

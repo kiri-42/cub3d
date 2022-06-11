9badc456e23bcc47f6c7a0f5139fc6b10716cf53
kiri0314 <t.kirihara0314@gmail.com>
2022-06-12 02:28:11 +0900

    feat: is_initialiized_texture_path

--- src/main.c
+++ src/main.c
@@ -6,6 +6,28 @@
 // 	printf("texture_path\n");
 // }
 
+void	is_initialized_texture_path(t_texture_path *tp)
+{
+	if (tp->no_path == NULL)
+		exit_error("no_path is uninitialized");
+	if (tp->so_path == NULL)
+		exit_error("so_path is uninitialized");
+	if (tp->we_path == NULL)
+		exit_error("we_path is uninitialized");
+	if (tp->ea_path == NULL)
+		exit_error("ea_path is uninitialized");
+}
+
+void	is_initialized(t_game_data *gd)
+{
+	is_initialized_texture_path(&(gd->tp));
+}
+
+void	check_game_data(t_game_data *gd)
+{
+	is_initialized(gd);
+}
+
 int	main(int ac, char **av)
 {
 	t_game_data	gd;
@@ -15,6 +37,7 @@ int	main(int ac, char **av)
 	read_cubfile(&gd, av[1]);
 	set_game_data(&gd);
 	free_cubfile(&gd);
+	check_game_data(&gd);
 	// check_texture_path(&gd);
 	check_map(&gd);
 	// print_texture_path(&gd);

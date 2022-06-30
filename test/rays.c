0a16a907dd0a45de5cae431f5111edefd530b416
astalow <endymion.0710@gmail.com>
2022-06-30 17:33:34 +0900

    Organize code

--- test/rays.c
+++ test/rays.c
@@ -2,13 +2,9 @@
 
 double	normalize_angle(double angle)
 {
-	// angle = angle % (2 * M_PI);
 	angle = fmod(angle, 2 * M_PI);
-	// angle = angle % (2*M_PI);
 	if (angle < 0)
-	{
 		angle = (2 * M_PI) + angle;
-	}
 	return (angle);
 }
 
@@ -23,17 +19,68 @@ t_direction get_direction_of_line(double angle)
 
 	ft_memset(&d, false, sizeof(t_direction));
 	if (angle > 0 && angle < M_PI)
-		d.down = 1;
-	if (d.down != 1)
-		d.up = 1;
+		d.down = true;
+	else
+		d.up = true;
 	if (angle < 0.5 * M_PI || angle > 1.5 * M_PI)
-		d.right = 1;
-	if (d.right != 1)
-		d.left = 1;
+		d.right = true;
+	else
+		d.left = true;
 	return (d);
 }
 
-double	horz_wall_distance(t_game_data *data, double angle, int id)
+t_coord	get_touch_point(t_game_data *data, t_coord intercept, t_coord step)
+{
+	t_coord touch;
+	t_coord	wall_hit;
+
+	touch.x = intercept.x;
+	touch.y = intercept.y;
+	while (touch.x >= 0 && touch.x <= WIDTH && touch.y >= 0 && touch.y <= HEIGHT)
+	{
+		if (has_wall(data->map, touch.x, touch.y))
+		{
+			wall_hit.x = touch.x;
+			wall_hit.y = touch.y;
+			draw_line(map, player.x, player.y, touch.x, touch.y);
+			return (wall_hit);
+		}
+		else
+		{
+			touch.x += step.x;
+			touch.y += step.y;
+		}
+	}
+	return (wall_hit);
+}
+
+t_coord	get_intercept(double angle, t_direction d)
+{
+	t_coord	intercept;
+
+	intercept.y = floor(player.y / TILE_SIZE) * TILE_SIZE;
+	if (d.down == 1)
+		intercept.y += TILE_SIZE;
+	intercept.x = player.x + (intercept.y - player.y) / tan(angle);
+	return (intercept);
+}
+
+t_coord	get_step(double angle, t_coord intercept, t_direction d)
+{
+	t_coord step;
+
+	step.y = TILE_SIZE;
+	if (d.up == 1)
+		step.y *= -1;
+	step.x = TILE_SIZE / tan(angle);
+	if (d.left == 1 && step.x > 0)
+		step.x *= -1;
+	if (d.right == 1 && step.x < 0)
+		step.x *= -1;
+	return (step);
+}
+
+t_coord	get_horz_wall_hit(t_game_data *data, double angle, int id)
 {
 	t_coord		wall_hit;
 	t_coord		intercept;
@@ -42,46 +89,11 @@ double	horz_wall_distance(t_game_data *data, double angle, int id)
 
 	angle = normalize_angle(angle);
 	d = get_direction_of_line(angle);
-
-	intercept.y = floor(player.y / TILE_SIZE) * TILE_SIZE;
-	if (d.down == 1)
-		intercept.y += TILE_SIZE;
-	intercept.x = player.x + (intercept.y - player.y) / tan(angle);
-
-	step.y = TILE_SIZE;
-	if (d.up == 1)
-		step.y *= -1;
-
-	step.x = TILE_SIZE / tan(angle);
-	if (d.left == 1 && step.x > 0)
-		step.x *= -1;
-	if (d.right == 1 && step.x < 0)
-		step.x *= -1;
-
-	double horz_touch_x = intercept.x;
-	double horz_touch_y = intercept.y;
-	bool	horz_wall_hit = false;
-
-	if (d.up)
-		horz_touch_y--;
-
-	while (horz_touch_x >= 0 && horz_touch_x <= WIDTH && horz_touch_y >= 0 && horz_touch_y <= HEIGHT)
-	{
-		if (has_wall(data->map, horz_touch_x, horz_touch_y))
-		{
-			horz_wall_hit = true;
-			wall_hit.x = horz_touch_x;
-			wall_hit.y = horz_touch_y;
-			draw_line(map, player.x, player.y, wall_hit.x, wall_hit.y);
-			break;
-		}
-		else
-		{
-			horz_touch_x += step.x;
-			horz_touch_y += step.y;
-		}
-	}
-	return (distance_between_points(player.x, player.y, wall_hit.x, wall_hit.y));
+	intercept = get_intercept(angle, d);
+	step = get_step(angle, intercept, d);
+	if (d.up == true)
+		intercept.y--;
+	return (get_touch_point(data, intercept, step));
 }
 
 void	cast_all_rays(t_game_data *data)
@@ -95,15 +107,9 @@ void	cast_all_rays(t_game_data *data)
 	column_id = 0;
 	rayAngle = player.rotationAngle - (FOV / 2);
 	i = 0;
-	while (i < RAYS)
+	while (i < 1)
 	{
-		d = horz_wall_distance(data, rayAngle, column_id);
-		printf("%f\n", d);
-		// cast(player.rotationAngle, 0);
-		// draw_line(map, player.x, player.y, player.x+cos(player.rotationAngle)*30, player.y+sin(player.rotationAngle)*30);
-		// return;
-		
-		// draw_line(map, player.x, player.y, player.x+cos(rayAngle)*30, player.y+sin(rayAngle)*30);
+		get_horz_wall_hit(data, rayAngle, column_id);
 		rayAngle += (FOV / RAYS);
 		i++;
 		column_id++;

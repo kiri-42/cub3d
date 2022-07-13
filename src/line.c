8be2d353ed018862878d32b176eec13854d9bcb1
astalow <endymion.0710@gmail.com>
2022-07-13 16:55:21 +0900

    add floor_coord

--- src/line.c
+++ src/line.c
@@ -6,6 +6,13 @@ int	to_chr_index(t_coord coord)
 	return ((int)round(coord.y) * WIDTH + (int)round(coord.x));
 }
 
+t_coord	floor_coord(t_coord coord)
+{
+	coord.x = floor(coord.x);
+	coord.y = floor(coord.y);
+	return (coord);
+}
+
 void	draw_line(t_game_data *data, t_coord start, t_coord goal, int color)
 {
 	double	delta_x;
@@ -13,10 +20,8 @@ void	draw_line(t_game_data *data, t_coord start, t_coord goal, int color)
 	double	step;
 	int		index;
 
-	start.x = floor(start.x);
-	start.y = floor(start.y);
-	goal.x = floor(goal.x);
-	goal.y = floor(goal.y);
+	start = floor_coord(start);
+	goal = floor_coord(goal);
 	delta_x = goal.x - start.x;
 	delta_y = goal.y - start.y;
 	step = (fabs(delta_x) > fabs(delta_y)) ? fabs(delta_x) : fabs(delta_y);

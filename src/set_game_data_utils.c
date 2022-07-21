9247dc45ce611a397ea270aabcb664040c44876b
kiri <t.kirihara0314@gmail.com>
2022-07-21 23:47:29 +0900

    norm対応

--- src/set_game_data_utils.c
+++ src/set_game_data_utils.c
@@ -2,10 +2,14 @@
 
 bool	is_path_line(char *str)
 {
-	return (!ft_strncmp(str, "NO", PREFIX_SIZE) || !ft_strncmp(str, "SO", PREFIX_SIZE) || !ft_strncmp(str, "WE", PREFIX_SIZE) || !ft_strncmp(str, "EA", PREFIX_SIZE));
+	return (!ft_strncmp(str, "NO", PREFIX_SIZE)
+		|| !ft_strncmp(str, "SO", PREFIX_SIZE)
+		|| !ft_strncmp(str, "WE", PREFIX_SIZE)
+		|| !ft_strncmp(str, "EA", PREFIX_SIZE));
 }
 
 bool	is_color_line(char *str)
 {
-	return (!ft_strncmp(str, "F ", PREFIX_SIZE) || !ft_strncmp(str, "C ", PREFIX_SIZE));
+	return (!ft_strncmp(str, "F ", PREFIX_SIZE)
+		|| !ft_strncmp(str, "C ", PREFIX_SIZE));
 }

3486e8aa90ee36e9180f2e0b1969b48d34f5c880
astalow <endymion.0710@gmail.com>
2022-07-28 18:34:13 +0900

    Split function

--- src/move.c
+++ src/move.c
@@ -1,14 +1,41 @@
 #include "cub3d.h"
 
-bool	is_wall(t_cell **map, t_position p)
+static bool	is_wall(t_game_data *gd, t_cell **map, t_position p)
 {
 	t_cell	m;
 
+	if ((size_t)p.y >= gd->rows || (size_t)p.x >= gd->cols)
+		return (true);
 	m = map[p.y][p.x];
 	return (m.type == MAP_WALL || (m.type == MAP_DOOR && m.door_open == CLOSE));
 }
 
-bool	is_reachable_diagonal(t_game_data *data, t_cell **map, \
+static void	set_diagonal_cell(t_position new, t_position player, \
+		t_position *p, t_position *p2)
+{
+	if (new.x > player.x && new.y > player.y)
+	{
+		(*p).x = player.x + 1;
+		(*p2).y = player.y + 1;
+	}
+	else if (new.x > player.x && new.y < player.y)
+	{
+		(*p).x = player.x + 1;
+		(*p2).y = player.y - 1;
+	}
+	else if (new.x < player.x && new.y < player.y)
+	{
+		(*p).x = player.x - 1;
+		(*p2).y = player.y - 1;
+	}
+	else if (new.x < player.x && new.y < player.y)
+	{
+		(*p).x = player.x - 1;
+		(*p2).y = player.y + 1;
+	}
+}
+
+bool	is_reachable_diagonal(t_game_data *gd, t_cell **map, \
 		t_position new, t_position player)
 {
 	t_position	p;
@@ -17,29 +44,10 @@ bool	is_reachable_diagonal(t_game_data *data, t_cell **map, \
 	p = player;
 	p2 = player;
 	if (new.x < 0 || new.y < 0 || \
-	(size_t)new.x >= data->cols || (size_t)new.y >= data->rows)
+	(size_t)new.x >= gd->cols || (size_t)new.y >= gd->rows)
 		return (false);
-	if (new.x > player.x && new.y > player.y)
-	{
-		p.x = player.x + 1;
-		p2.y = player.y + 1;
-	}
-	else if (new.x > player.x && new.y < player.y)
-	{
-		p.x = player.x + 1;
-		p2.y = player.y - 1;
-	}
-	else if (new.x < player.x && new.y < player.y)
-	{
-		p.x = player.x - 1;
-		p2.y = player.y - 1;
-	}
-	else if (new.x < player.x && new.y < player.y)
-	{
-		p.x = player.x - 1;
-		p2.y = player.y + 1;
-	}
-	if (is_wall(map, p) == true && is_wall(map, p2) == true)
+	set_diagonal_cell(new, player, &p, &p2);
+	if (is_wall(gd, map, p) == true && is_wall(gd, map, p2) == true)
 		return (false);
 	return (true);
 }
@@ -55,7 +63,7 @@ bool	is_reachable(t_game_data *gd, t_cell **map, t_coord coord)
 		return (true);
 	if ((size_t)new.x >= gd->cols || (size_t)new.y >= gd->rows)
 		return (false);
-	if (is_wall(map, new))
+	if (is_wall(gd, map, new))
 		return (false);
 	if (!is_reachable_diagonal(gd, map, new, player))
 		return (false);
